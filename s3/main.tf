e0651a084c9ed52b4bf4ebdd159b91981fe7451c
jordanvieira <marvaojordan98@gmail.com>
2022-01-13 12:16:29 -0300

    Criando Bucket S3

--- s3/main.tf
+++ s3/main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  region = "us-east-1"
-  profile = ""//credencias aws-cli
+  region  = "us-east-1"
+  profile = "" //credencias aws-cli
 }
 
 resource "aws_s3_bucket" "my-test-bucket" {
 
   bucket = "my-test-bucket-test"
-  acl = "private"
+  acl    = "private"
 
   tags = {
 
-      Name = "my bucket"
-      Environment = "Dev"
-      Manageby = "Terraform"
+    Name        = "my bucket"
+    Environment = "Dev"
+    Manageby    = "Terraform"
   }
 
 }
