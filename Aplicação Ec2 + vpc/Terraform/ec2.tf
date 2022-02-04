ab2b9b5499ab6bca720ea9fcc38febea1cac49c3
Jordan Vieira <84735055+jordanvieira@users.noreply.github.com>
2022-02-04 15:12:12 -0300

    Aplicação Ec2 e VPC
    EC2 sql server, ubuntu e VPC

---
+++ Aplicação Ec2 + vpc/Terraform/ec2.tf
@@ -0,0 +1,54 @@
+# resource "aws_volume_attachment" "sql_server_attach" {
+#   device_name = "/dev/sdh"
+#   volume_id   = aws_ebs_volume.sql_server_ebs.id
+#   instance_id = aws_instance.sql_server.id
+# }
+
+# resource "aws_ebs_volume" "sql_server_ebs" {
+#   availability_zone = "${var.aws_region}a"
+#   size              = 512
+#   type              = "gp3"
+
+# }
+
+#Criando Instancia EC2 - WIN_SQL_SERVER
+resource "aws_instance" "sql_server" {
+  ami           = "ami-0aad84f764a2bd39a"
+  instance_type = "t3a.xlarge"
+  key_name      = "terraform"
+
+
+  subnet_id                   = aws_subnet.private.0.id
+  availability_zone           = "${var.aws_region}a"
+  vpc_security_group_ids      = [aws_security_group.sql_server.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 512
+    volume_type = "gp3"
+  }
+
+
+  tags = merge(local.common_tags, { Name = "Terraform_Win_Sql_Server" })
+
+}
+
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t3a.small"
+  key_name      = "terraform"
+
+  subnet_id              = aws_subnet.public.0.id
+  availability_zone      = "${var.aws_region}a"
+  vpc_security_group_ids = [aws_security_group.vpn.id]
+
+    root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+
+  tags = merge(local.common_tags, { Name = "Terraform_ubuntu_VPN" })
+
+}
---
+++ Aplicação Ec2 + vpc/Terraform/security_group.tf
@@ -0,0 +1,62 @@
+resource "aws_security_group" "sql_server" {
+  name        = "sql_server"
+  description = "sql_server machine"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, { Name = "Sql_server" })
+}
+
+
+
+resource "aws_security_group" "vpn" {
+  name        = "vpn"
+  description = "vpn machine"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 10447
+    to_port     = 10447
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, { Name = "Sql_server" })
+}
+
